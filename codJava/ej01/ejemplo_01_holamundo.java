1472fd5f12dbbbda55e0e061a905728536bee652
Curso Desarrollo <ifcd0112.avante.2025@gmail.com>
2025-07-16 14:33:28 +0200

    20250719 Ejemplo Java

---
+++ codJava/ej01/ejemplo_01_holamundo.java
@@ -0,0 +1,9 @@
+// Paquete -> Carpeta
+package codJava.ej01;
+
+/* Clase -> nombre del archivo sin .java*/ 
+public class ejemplo_01_holamundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
