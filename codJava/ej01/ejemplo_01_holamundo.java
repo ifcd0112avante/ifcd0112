868e3a7f5b3c2f0ed68a43364c647fbf50598dfd
Iván Rodríguez Ruiz <ivanrguez1@yahoo.es>
2025-07-21 09:21:27 +0200

    20250721 Actualización ejemplos Java

--- codJava/ej01/ejemplo_01_holamundo.java
+++ codJava/ej01/ejemplo_01_holamundo.java
@@ -2,6 +2,9 @@
 package codJava.ej01;
 
 /* Clase -> nombre del archivo sin .java*/ 
+/* API JAVA21 -> https://docs.oracle.com/en/java/javase/21/docs/api/index.html 
+ * Ej: JAVADOC -> https://docs.oracle.com/en/java/javase/21/docs/api/jdk.javadoc/module-summary.html
+*/
 public class ejemplo_01_holamundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
